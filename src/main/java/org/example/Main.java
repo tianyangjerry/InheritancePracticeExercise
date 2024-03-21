aa2420e1246ec4a74356c765b7a9b2f568e14444
TianYangChen <159708159+tianyangjerry@users.noreply.github.com>
2024-03-21 17:07:38 +0800

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 import java.util.Scanner;
 public class Main {
+    static public double sum = 0.0;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("How many events would you like to organize?");
@@ -47,9 +48,12 @@ public class Main {
                     System.out.println("Enter the cost of organizing dinner: ");
                     dinnerCost = scanner.nextDouble();
                 }
-                ConferenceEvent conferenceEvent = new ConferenceEvent(eventID, eventName, eventLocation, eventPointOfContact, eventCost, totalParticipants, totalEventDays, breakfastRequired, breakfastCost, lunchRequired, lunchCost, dinnerRequired, dinnerCost);
+                ConferenceEvent conferenceEvent = new ConferenceEvent(eventID, eventName, eventLocation,
+                        eventPointOfContact, eventCost, totalParticipants, totalEventDays,
+                        breakfastRequired, breakfastCost, lunchRequired, lunchCost, dinnerRequired, dinnerCost);
+                sum += conferenceEvent.calculateEventCost();
                 System.out.println(conferenceEvent);
-                System.out.println("The total cost of organizing the conference event is: " + conferenceEvent.calculateEventCost());
+                //System.out.println("The total cost of organizing the conference event is: " + conferenceEvent.calculateEventCost());
             }
             else if (eventType == 2) { //choose music concert event
                 System.out.println("Enter the event ID: ");
@@ -73,10 +77,14 @@ public class Main {
                     System.out.println("Enter the cost of providing merchandise: ");
                     merchandiseCost = scanner.nextDouble();
                 }
-                MusicConcertEvent musicConcertEvent = new MusicConcertEvent(eventID, eventName, eventLocation, eventPointOfContact, eventCost, totalParticipants, totalEventDays, merchandiseRequired, merchandiseCost);
+                MusicConcertEvent musicConcertEvent = new MusicConcertEvent(eventID, eventName,
+                        eventLocation, eventPointOfContact, eventCost, totalParticipants,
+                        totalEventDays, merchandiseRequired, merchandiseCost);
+                sum += musicConcertEvent.calculateEventCost();
                 System.out.println(musicConcertEvent);
-                System.out.println("The total cost of organizing the music concert event is: " + musicConcertEvent.calculateEventCost());
+                //System.out.println("The total cost of organizing the music concert event is: " + musicConcertEvent.calculateEventCost());
             }
         }
+        System.out.println("The total cost of organizing all events is: " + sum);
     }
 }
